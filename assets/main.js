c2d6882d612ccb68760d2d08e3da1b5e607bf4ec
Haicheng Charles Zhao <czhao39@gmail.com>
2017-02-25 11:57:36 -0500

    create hero

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(".dropdown-button").dropdown({hover: false});
+
+    $(".carousel.carousel-slider").carousel({fullWidth: true, indicators: true, duration: 100});
+});
