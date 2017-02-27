b7eda3416d1b9e35237f0f79d6e0016ca7734dcd
Haicheng Charles Zhao <czhao39@gmail.com>
2017-02-26 21:13:14 -0500

    fix slider scaling on mobile

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(".dropdown-button").dropdown({hover: false});
 
-    $(".slider").slider({interval: 5000});
+    $(".slider").slider({interval: 5000, height: null});
 
     $("#hero-title.typed").typed({
         strings: ["AIM Robotics"],
@@ -9,9 +9,14 @@ $(document).ready(function() {
     });
 
     $window = $(window);
+    var height = Math.max($window.height(), $(".slider-text").height()+200);
+    $(".slider").height(height - 40);
+    $(".slider .slides").height(height - 40);
+
+
     $navbar = $(".fade-color-nav");
     $window.scroll(function() {
-        var frac = $window.scrollTop() / $window.height();
+        var frac = $window.scrollTop() / height;
         if (frac > 1)
             frac = 1;
         $navbar.css("background-color", "rgb(" + Math.round(13*frac).toString() + ", " + Math.round(71*frac).toString() + ", " + Math.round(161*frac).toString() + ")");
