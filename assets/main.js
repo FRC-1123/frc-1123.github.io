0ab8ef2b59076302b4e2871f877531c253d06066
Haicheng Charles Zhao <czhao39@gmail.com>
2017-02-27 16:20:25 -0500

    full-width parallax images

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,20 @@ $(document).ready(function() {
 
     $(".to-underline").addClass("underline");
 
+    var $window = $(window);
     $(".parallax").parallax();
+    $window.resize(function() {
+        var width = $window.width();
+        $(".parallax-container").width(width);
+        if (width <= 600)
+            $(".parallax-container").css("left", -width*0.05 + "px");
+        else if (width <= 992)
+            $(".parallax-container").css("left", -width*0.075 + "px");
+        else
+            $(".parallax-container").css("left", -width*0.15 + "px");
+    });
+    $(".parallax-container").css("position", "relative");
+    $window.resize();
 
     var sr = ScrollReveal();
     sr.reveal("#members .card", {rotate: {z: 4}, easing: "ease-out", duration: 500});
