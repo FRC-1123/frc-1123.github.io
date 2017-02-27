26e6e28530bfabeadd59228a3182ed73542dfc99
Haicheng Charles Zhao <czhao39@gmail.com>
2017-02-26 22:16:49 -0500

    add entrance animation to member cards

--- assets/main.js
+++ assets/main.js
@@ -4,4 +4,7 @@ $(document).ready(function() {
     $(".to-underline").addClass("underline");
 
     $(".parallax").parallax();
+
+    var sr = ScrollReveal();
+    sr.reveal("#members .card", {rotate: {z: 4}, easing: "ease-out", duration: 500});
 });
